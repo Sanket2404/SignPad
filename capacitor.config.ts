5e50d1ebf37e734eb1b478527f5938ae4a894b79
Sanket_2404 <sanketj24.rts@gmail.com>
2024-10-30 13:23:10 +0530

    initial comment

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Sign_pad',
+  webDir: 'www'
+};
+
+export default config;
---
+++ src/app/components/sign-pad/sign-pad.component.ts
@@ -0,0 +1,47 @@
+import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { SignaturePad } from 'angular2-signaturepad';
+
+@Component({
+  selector: 'app-sign-pad',
+  templateUrl: './sign-pad.component.html',
+  styleUrls: ['./sign-pad.component.scss'],
+})
+export class SignPadComponent {
+  @ViewChild(SignaturePad) signaturePad!: SignaturePad;
+
+  signaturePadOptions: Object = { // passed through to szimek/signature_pad constructor
+    'minWidth': 5,
+    // 'canvasWidth': 360,
+    // 'canvasHeight': 500
+  };
+  @Output() signatureImage: EventEmitter<string>= new EventEmitter<string>;
+  constructor() { }
+
+  ngAfterViewInit() {
+    this.drawClear();
+    this.canvasResize();
+  }
+
+  canvasResize(){
+    let canvas: HTMLCanvasElement | null = document.querySelector('canvas');
+    console.log(window.innerHeight);
+    if(canvas){
+    canvas.height = window.innerHeight - (0.19 * window.innerHeight)
+    canvas.width = window.innerWidth;
+    }
+  }
+
+  drawComplete(){
+    this.signatureImage.emit(this.signaturePad.toDataURL());
+    console.log('====================this.signatureImage=================',this.signatureImage);
+  }
+
+  drawStart() {
+    console.log('begin drawing');
+  }
+
+  drawClear(){
+    this.signaturePad.clear();
+  }
+
+}
